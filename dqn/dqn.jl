f69a8480f42df615e8fa08fd52f347bd9de99260
Tejan Karmali <tejank10@gmail.com>
2018-07-15 10:15:10 -0400

    changed algo to openai winner

--- dqn/dqn.jl
+++ dqn/dqn.jl
@@ -19,39 +19,35 @@ env = GymEnv("CartPole-v0")
 
 STATE_SIZE = length(env.state)
 ACTION_SIZE = length(env.actions)
-MEM_SIZE = 1000000
-BATCH_SIZE = 32
-REPLAY_START_SIZE = 50000
+MEM_SIZE = 100000
+BATCH_SIZE = 64
 UPDATE_FREQ = 10000
-γ = 0.99    # discount rate
+γ = 1.0f0   # discount rate
 
 # Exploration params
-ϵ_START = 1.0   # Initial exploration rate
-ϵ_STOP = 0.1    # Final exploratin rate
-ϵ_STEPS = 1000000   # Final exploration frame, using linear annealing
+ϵ = 1.0f0   # Initial exploration rate
+ϵ_STOP = 0.01f0    # Final exploratin rate
+ϵ_DECAY = 0.995f0
 
 # Optimiser params
-η = 2.5e-4   # Learning rate
-ρ = 0.95    # Gradient momentum for RMSProp
+η = 0.01f0   # Learning rate
 
 memory = [] #used to remember past results
-frames = 0
-C = 0
 
 # ------------------------------ Model Architecture ----------------------------
 
-model = Chain(Dense(STATE_SIZE, 24, σ), Dense(24, 24, σ), Dense(24, ACTION_SIZE))
+model = Chain(Dense(STATE_SIZE, 24, tanh), Dense(24, 48, tanh), Dense(48, ACTION_SIZE)) |> gpu
 model_target = deepcopy(model)
 
 loss(x, y) = Flux.mse(model(x), y)
 
-opt = RMSProp(params(model), η; ρ = ρ)
+opt = ADAM(params(model), η; decay = 0.01f0)
 
 fit_model(dataset) = Flux.train!(loss, dataset, opt)
 
 # ----------------------------- Helper Functions -------------------------------
 
-get_ϵ() = frames >= ϵ_STEPS ? ϵ_STOP : ϵ_START + frames * (ϵ_STOP - ϵ_START) / ϵ_STEPS
+get_ϵ(e) = max(ϵ_STOP, min(ϵ, 1.0f0-log10(e * ϵ_DECAY)))
 
 function remember(state, action, reward, next_state, done)
   if length(memory) == MEM_SIZE
@@ -61,46 +57,45 @@ function remember(state, action, reward, next_state, done)
 end
 
 function action(π::CartPolePolicy, reward, state, action)
-  if rand() <= get_ϵ() && π.train
+  if rand() <= get_ϵ(e) && π.train
     return rand(1:ACTION_SIZE) - 1
   end
 
-  act_values = model(state)
+  act_values = model(state |> gpu)
   return Flux.argmax(act_values) - 1
 end
 
 function replay()
-  global model_target, C
+  global ϵ, ϵ_STOP, ϵ_DECAY
   minibatch = sample(memory, BATCH_SIZE, replace = false)
-
-  for (state, action, reward, next_state, done) in minibatch
+  x = Matrix{Float32}(STATE_SIZE, BATCH_SIZE)
+  y = Matrix{Float32}(ACTION_SIZE, BATCH_SIZE)
+  for (iter, (state, action, reward, next_state, done)) in enumerate(minibatch)
     target = reward
     if !done
-      target += γ * maximum(model_target(next_state).data)
+      target += γ * maximum(model(next_state |> gpu).data)
     end
 
-    target_f = model(state).data
+    target_f = model(state |> gpu).data
     target_f[action] = target
-    dataset = zip(state, target_f)
-    fit_model(dataset)
-
-    if C == 0
-      model_target = deepcopy(model)
-    end
-
-    C = (C + 1) % UPDATE_FREQ
+    
+    x[:, iter] .= state
+    y[:, iter] .= target_f
+  end
+  x = x |> gpu
+  y = y |> gpu
+  fit_model([(x, y)])
+  if ϵ > ϵ_STOP
+    ϵ *= ϵ_DECAY
   end
 end
 
 function episode!(env, π = RandomPolicy())
-  global frames
   ep = Episode(env, π)
 
   for (s, a, r, s′) in ep
-    OpenAIGym.render(env)
-    r = env.done ? -1 : r
+    #OpenAIGym.render(env)
     if π.train remember(s, a + 1, r, s′, env.done) end
-    frames += 1
   end
 
   ep.total_reward
@@ -109,11 +104,11 @@ end
 # ------------------------------ Training --------------------------------------
 
 e = 1
-while frames < ϵ_STEPS
+while e < 1000
   reset!(env)
   total_reward = episode!(env, CartPolePolicy())
   println("Episode: $e | Score: $total_reward")
-  if length(memory) >= REPLAY_START_SIZE
+  if length(memory) > BATCH_SIZE
     replay()
   end
   e += 1
